e8ac5915ea6add156efa7e9062f6cc773e7cea28
Cappuccnio <91250379+kapumochi@users.noreply.github.com>
2021-09-30 17:45:28 +0800

    Add files via upload

---
+++ week03-3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello%dWorld\n", 300);
+
+    printf("%d %d  %d", 300, 200, 100);
+}
+///%d decimal digit (10進位的整數)
+///也會依照同一行的先後順序輸出
+
---
+++ week03-4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+    printf("請輸入一個數字 : ");
+
+    scanf("%d" , &a); ///在輸出視窗打數字
+	printf("你剛剛輸入的是 : %d", a);
+}
