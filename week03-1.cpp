be2644b9051bd881a2321eaf15766c615ff91b5f
Cappuccnio <91250379+kapumochi@users.noreply.github.com>
2021-10-03 02:05:58 +0800

    Add files via upload

---
+++ week03-1.cpp
@@ -0,0 +1,14 @@
+///(1)開新檔案 File >> New >> Empty file (Ctrl+Shift+N)
+///(2)要把它分類 先存檔 (Save As) 就會變色
+ #include <stdio.h>
+///stdio = Standard Output Input
+int main()
+{
+    printf("Hello World");  ///沒換行
+    printf("bruh\n");   ///換一行
+    printf("Hello World\n\n\n\n\n\n\n\n\n");    ///瘋狂換行
+}
+///小括號是類似數學函式的功能
+///大括號是把很多函式包起來
+///行列數旁邊出現紅色表示那行是錯誤的語法
+///反斜線\n = new line (新的一行)
