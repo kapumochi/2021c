996c3339a13c33bb078d8d6918e8964333ed3ca2
Cappuccnio <91250379+kapumochi@users.noreply.github.com>
2021-10-07 18:59:58 +0800

    Add files via upload

---
+++ week04-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a=100,b=99;
+    printf("%d\n",a+b);
+    printf("%5d\n",a+b);
+    float c=3.14159265358579;
+    double d=3.14159265358579;
+    printf("%f\n", c);
+    printf("%f\n", d);
+    printf("%.4f\n", d);
+    printf("%.9f\n", d);
+}
