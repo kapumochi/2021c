ae8648f53a588e788fcf9191d3df884c0b9db6b9
kapumochi <chen38587@gmail.com>
2021-12-09 17:48:27 +0800

    這次我更新week 13喔

---
+++ week13-1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("plz type ur seat number 1~10 ");
+    int id;
+    scanf("%d", &id);
+
+    printf("%5s %5s %5s %5s %5s %5s %5s %5s %5s\n","name","No.","國文","數學","英文","機電","電子","總分","平均成績");
+    printf("%5s %5d %5d %5d %5d %5d %5d %5d %6.2f\n","名子1",1,1,1,1,1,1,5,1.0f);
+    printf("%5s %5d %5d %5d %5d %5d %5d %5d %6.2f\n","名子5",5,100,100,100,100,100,500,100.0f);
+}
+
---
+++ week13-3.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void printStar(int n){
+    for(int i=0; i<n; i++)printf("*");
+}
+int main(){
+    printStar(7);
+}
